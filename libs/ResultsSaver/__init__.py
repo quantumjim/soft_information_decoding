c90bcb285f32378165e5ad7583746af4668d85b3
Maurice D. Hanisch <maurice.hanisch@ibm.com>
2023-10-23 19:45:18 +0200

    finished result_saver

---
+++ libs/ResultsSaver/__init__.py
@@ -0,0 +1,1 @@
+from result_saver import *
--- libs/ResultsSaver/result_saver/provider/metadata_provider.py
+++ libs/ResultsSaver/result_saver/provider/metadata_provider.py
@@ -3,8 +3,35 @@
 
 from qiskit_ibm_provider import IBMProvider
 from qiskit.providers.backend import BackendV1 as Backend
-from typing import Optional, Any
+from typing import Optional, Any, Union
 
+def metadata_helper(n_shots: Union[int, float], meas_level: Union[1, 2], *args, **kwargs):
+    """
+    A helper function to collect and organize metadata.
+
+    Parameters:
+    n_shots (Union[int, float]): The number of shots for the experiment.
+    meas_level (Union[1, 2]): The measurement level, either 1 or 2.
+    *args: Additional arguments.
+    **kwargs: Additional keyword arguments.
+
+    Returns:
+    Dict[str, Any]: A dictionary containing the collected metadata.
+    """
+
+    # Organize the metadata into a dictionary
+    metadata = {
+        'n_shots': n_shots,
+        'meas_level': meas_level
+    }
+
+    # If there are additional arguments or keyword arguments, include them in the metadata
+    if args:
+        metadata['additional_args'] = args
+    if kwargs:
+        metadata.update(kwargs)
+
+    return metadata
 
 class MetadataProvider(IBMProvider):
 
