72b5966debae30b4fac5b84f3ab0e827f8cf8a6d
Maurice D. Hanisch <maurice.hanisch@ibm.com>
2023-10-28 17:48:52 +0200

    Custom warnings to Scratch & soft_info __init__.py

--- src/Scratch/__init__.py
+++ src/Scratch/__init__.py
@@ -1,4 +1,16 @@
+import warnings
+import datetime
+
 from .core import *
 from .qubit_coordinates import *
-from .metadata import * 
+from .metadata import *
 from .calibration_data import *
+
+
+def custom_showwarning(message, category, filename, lineno, file=None, line=None):
+    datetime_str = datetime.datetime.now().strftime("%H:%M:%S")
+    print(f"{datetime_str} Warning: {message}. IN FILE: {filename}, LINE: {lineno}")
+
+
+# Override the default showwarning function
+warnings.showwarning = custom_showwarning
--- src/soft_info/__init__.py
+++ src/soft_info/__init__.py
@@ -1,3 +1,15 @@
+import warnings
+import datetime
+
 from .IQ_data import *
 from .UnionFind import *
 from .Hardware import *
+
+
+def custom_showwarning(message, category, filename, lineno, file=None, line=None):
+    datetime_str = datetime.datetime.now().strftime("%H:%M:%S")
+    print(f"{datetime_str} Warning: {message}. IN FILE: {filename}, LINE: {lineno}")
+
+
+# Override the default showwarning function
+warnings.showwarning = custom_showwarning
