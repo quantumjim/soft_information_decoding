f7901985bbd7aa52608b194208d15b696c3e3033
Maurice D. Hanisch <maurice.hanisch@ibm.com>
2023-11-26 19:12:43 +0100

    Added PM and Stim as folders (not submodules)

---
+++ libs/PyMatching/Stim/src/stim/stabilizers/pauli_string.pybind.h
@@ -0,0 +1,64 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef _STIM_STABILIZERS_PAULI_STRING_PYBIND_H
+#define _STIM_STABILIZERS_PAULI_STRING_PYBIND_H
+
+#include <complex>
+#include <pybind11/pybind11.h>
+
+#include "stim/stabilizers/pauli_string.h"
+
+namespace stim_pybind {
+
+struct PyPauliString {
+    stim::PauliString<stim::MAX_BITWORD_WIDTH> value;
+    bool imag;
+
+    PyPauliString(const stim::PauliStringRef<stim::MAX_BITWORD_WIDTH> val, bool imag = false);
+    PyPauliString(stim::PauliString<stim::MAX_BITWORD_WIDTH> &&val, bool imag = false);
+
+    static PyPauliString from_text(const char *c);
+    std::complex<float> get_phase() const;
+
+    PyPauliString operator+(const PyPauliString &rhs) const;
+    PyPauliString &operator+=(const PyPauliString &rhs);
+
+    PyPauliString operator*(const pybind11::object &rhs) const;
+    PyPauliString operator*(size_t power) const;
+    PyPauliString operator*(std::complex<float> scale) const;
+    PyPauliString operator*(const PyPauliString &rhs) const;
+    PyPauliString operator/(const std::complex<float> &divisor) const;
+
+    PyPauliString &operator*=(const pybind11::object &rhs);
+    PyPauliString &operator*=(size_t power);
+    PyPauliString &operator*=(std::complex<float> scale);
+    PyPauliString &operator*=(const PyPauliString &rhs);
+    PyPauliString &operator/=(const std::complex<float> &divisor);
+
+    pybind11::object to_unitary_matrix(const std::string &endian) const;
+    static PyPauliString from_unitary_matrix(
+        const pybind11::object &matrix, const std::string &endian, bool ignore_sign);
+
+    bool operator==(const PyPauliString &other) const;
+    bool operator!=(const PyPauliString &other) const;
+    std::string str() const;
+};
+
+pybind11::class_<PyPauliString> pybind_pauli_string(pybind11::module &m);
+void pybind_pauli_string_methods(pybind11::module &m, pybind11::class_<PyPauliString> &c);
+
+}  // namespace stim_pybind
+
+#endif
---
+++ libs/README.md
@@ -0,0 +1,6 @@
+## Warning
+
+PyMatching is cloned from https://github.com/oscarhiggott/PyMatching.git the 26.11.23 
+/PyMatching/Stim is cloned from https://github.com/quantumlib/Stim.git the 26.11.23
+
+To be able to run workflows with PyMatching and Stim they are not added as submodules but as folders in the libs folder.
